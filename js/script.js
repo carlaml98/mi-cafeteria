662aed7dc379368ee44579acf73ee4381a0df85d
carlaml98 <carlamarialuque@gmail.com>
2026-04-28 11:25:18 -0300

    javascript basico agregado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,28 @@ function toggleModo() {
         btn.textContent = '🌙';
     }
 }
+
 function toggleMenu() {
     const nav = document.getElementById('nav-menu');
     nav.classList.toggle('abierto');
 }
+
+window.addEventListener("load", function() {
+    let contador = 0;
+
+    const botonMenu = document.getElementById("btn-menu");
+    const hero = document.querySelector(".hero");
+
+    // Crear el contador visual
+    const contadorTexto = document.createElement("p");
+    contadorTexto.id = "contador";
+    contadorTexto.style.color = "brown";
+    contadorTexto.style.marginTop = "10px";
+    contadorTexto.textContent = "Visitas al menú: 0";
+    hero.appendChild(contadorTexto);
+
+    botonMenu.addEventListener("click", function() {
+        contador++;
+        contadorTexto.textContent = "Visitas al menú: " + contador;
+    });
+});
